70f1466438956e42e2e69d57e0b7ab5605af710a
Bhargavi1501 <balabhargavi39@gmail.com>
2023-07-13 10:35:13 -0400

    agecalculator

---
+++ index.js
@@ -0,0 +1,16 @@
+
+function calculateAge(){
+    const day = document.getElementById("day");
+    const month = document.getElementById("month");
+    const year = document.getElementById("year");
+    
+    console.log(day.value);
+    
+    
+    console.log(month.value);
+    
+    
+    console.log(year.value);
+}
+
+
